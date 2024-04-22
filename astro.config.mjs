28029fb06ec36432c0c0adadaf782dfbf3985638
Jenniferwonder <Jenniferwonder@163.com>
2024-04-22 16:37:16 +0800

    fix(temp): reuse astro-swup to resolve page switch delay

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,110 +1,110 @@
-import svelte from '@astrojs/svelte'
-import tailwind from '@astrojs/tailwind'
-import swup from '@swup/astro'
-import Compress from 'astro-compress'
-import icon from 'astro-icon'
-import { defineConfig } from 'astro/config'
-import Color from 'colorjs.io'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import rehypeKatex from 'rehype-katex'
-import rehypeSlug from 'rehype-slug'
-import remarkMath from 'remark-math'
-import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs'
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+import swup from "@swup/astro";
+import Compress from "astro-compress";
+import icon from "astro-icon";
+import { defineConfig } from "astro/config";
+import Color from "colorjs.io";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeKatex from "rehype-katex";
+import rehypeSlug from "rehype-slug";
+import remarkMath from "remark-math";
+import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
-const oklchToHex = str => {
-  const DEFAULT_HUE = 250
-  const regex = /-?\d+(\.\d+)?/g
-  const matches = str.string.match(regex)
-  const lch = [matches[0], matches[1], DEFAULT_HUE]
-  return new Color('oklch', lch).to('srgb').toString({
-    format: 'hex',
-  })
-}
+const oklchToHex = (str) => {
+	const DEFAULT_HUE = 250;
+	const regex = /-?\d+(\.\d+)?/g;
+	const matches = str.string.match(regex);
+	const lch = [matches[0], matches[1], DEFAULT_HUE];
+	return new Color("oklch", lch).to("srgb").toString({
+		format: "hex",
+	});
+};
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jenniferwonder.github.io/technote',
-  base: '/technote',
-  prefetch: {
-    defaultStrategy: 'hover',
-    prefetchAll: true,
-  },
-  experimental: {
-    contentCollectionCache: true,
-  },
-  // i18n: {
-  //   defaultLocale: "en",
-  //   locales: ["en", "es", "pt-br"],
-  //   routing: {
-  //     prefixDefaultLocale: false
-  // }
-  // },
-  integrations: [
-    tailwind(),
-    //Ref: https://github.com/swup/astro?tab=readme-ov-file#configuration
-    // swup({
-    //   theme: false,
-    //   animationClass: 'transition-',
-    //   containers: ['main'],
-    //   smoothScrolling: true,
-    //   cache: true,
-    //   preload: true,
-    //   accessibility: true,
-    //   globalInstance: true,
-    //   loadOnIdle: false,
-    // }),
-    icon({
-      include: {
-        'material-symbols': ['*'],
-        'fa6-brands': ['*'],
-        'fa6-regular': ['*'],
-        'fa6-solid': ['*'],
-      },
-    }),
-    Compress({
-      Image: false,
-    }),
-    svelte(),
-  ],
-  markdown: {
-    remarkPlugins: [remarkMath, remarkReadingTime],
-    rehypePlugins: [
-      rehypeKatex,
-      rehypeSlug,
-      [
-        rehypeAutolinkHeadings,
-        {
-          behavior: 'append',
-          properties: {
-            className: ['anchor'],
-          },
-          content: {
-            type: 'element',
-            tagName: 'span',
-            properties: {
-              className: ['anchor-icon'],
-              'data-pagefind-ignore': true,
-            },
-            children: [
-              {
-                type: 'text',
-                value: '#',
-              },
-            ],
-          },
-        },
-      ],
-    ],
-  },
-  vite: {
-    css: {
-      preprocessorOptions: {
-        stylus: {
-          define: {
-            oklchToHex: oklchToHex,
-          },
-        },
-      },
-    },
-  },
-})
+	site: "https://jenniferwonder.github.io/technote",
+	base: "/technote",
+	prefetch: {
+		defaultStrategy: "hover",
+		prefetchAll: true,
+	},
+	experimental: {
+		contentCollectionCache: true,
+	},
+	// i18n: {
+	//   defaultLocale: "en",
+	//   locales: ["en", "es", "pt-br"],
+	//   routing: {
+	//     prefixDefaultLocale: false
+	// }
+	// },
+	integrations: [
+		tailwind(),
+		//Ref: https://github.com/swup/astro?tab=readme-ov-file#configuration
+		swup({
+			theme: false,
+			animationClass: "transition-",
+			containers: ["main"],
+			smoothScrolling: true,
+			cache: true,
+			preload: true,
+			accessibility: true,
+			globalInstance: true,
+			loadOnIdle: false,
+		}),
+		icon({
+			include: {
+				"material-symbols": ["*"],
+				"fa6-brands": ["*"],
+				"fa6-regular": ["*"],
+				"fa6-solid": ["*"],
+			},
+		}),
+		Compress({
+			Image: false,
+		}),
+		svelte(),
+	],
+	markdown: {
+		remarkPlugins: [remarkMath, remarkReadingTime],
+		rehypePlugins: [
+			rehypeKatex,
+			rehypeSlug,
+			[
+				rehypeAutolinkHeadings,
+				{
+					behavior: "append",
+					properties: {
+						className: ["anchor"],
+					},
+					content: {
+						type: "element",
+						tagName: "span",
+						properties: {
+							className: ["anchor-icon"],
+							"data-pagefind-ignore": true,
+						},
+						children: [
+							{
+								type: "text",
+								value: "#",
+							},
+						],
+					},
+				},
+			],
+		],
+	},
+	vite: {
+		css: {
+			preprocessorOptions: {
+				stylus: {
+					define: {
+						oklchToHex: oklchToHex,
+					},
+				},
+			},
+		},
+	},
+});
